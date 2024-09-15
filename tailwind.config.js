a011d38d27d4728d392c815f79ba304ff9b5841f
Sarah Guadalupe Martínez Navarro <A01703113@tec.mx>
2024-09-15 01:03:52 -0600

    Establish frontend basis and rules

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,19 @@ module.exports = {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        nav: "#55AD9B",
+        page: "#F1F8E8",
+        card: "#95D2B3",
+        "card-hover": "#D8EFD3",
+        "default-text": "#17153B",
+        "blue-accent": "#55AD9B",
+        "blue-accent-hover": "#D8EFD3",
       },
     },
   },
